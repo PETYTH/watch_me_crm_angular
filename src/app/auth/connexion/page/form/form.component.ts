d284c4a6a41ddfde11adb62f8d9f7cad1315eb69
CEO-Niels <nielsbeaumont.etu@gmail.com>
2023-11-19 14:23:37 +0100

    routerlink ok

--- src/app/auth/connexion/page/form/form.component.ts
+++ src/app/auth/connexion/page/form/form.component.ts
@@ -5,7 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
+
 export class FormComponent {
-  
- 
+  email: string = '';
+  password: string = '';
+
+  onSubmit() {
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+  }
 }
