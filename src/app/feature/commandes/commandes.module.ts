bcb9dcdcab6a0772851de4567dac11d367b0f7d2
Christophe <kelevrap5@gmail.com>
2023-12-01 00:48:27 +0100

    continuation responsive dashboard home

--- src/app/feature/commandes/commandes.module.ts
+++ src/app/feature/commandes/commandes.module.ts
@@ -4,16 +4,18 @@ import { CommandesRoutingModule } from './commandes-routing.module';
 import { RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
 import {ListComponent} from "./page/list/list.component";
+import {SharedModule} from "../../shared/shared.module";
 
 @NgModule({
   declarations: [
     ListComponent
   ],
-  imports: [
-    CommandesRoutingModule,
-    RouterModule,
-    CommonModule,
-  ]
+    imports: [
+        CommandesRoutingModule,
+        RouterModule,
+        CommonModule,
+        SharedModule,
+    ]
 })
 
 export class CommandesModule { }
