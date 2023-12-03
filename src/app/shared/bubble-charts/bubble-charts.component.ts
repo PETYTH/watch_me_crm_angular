88365fee75895698b7ec5ef5c14ef129651fcf08
Christophe <kelevrap5@gmail.com>
2023-12-03 20:58:42 +0100

    fin responsive home dashboard

--- src/app/shared/bubble-charts/bubble-charts.component.ts
+++ src/app/shared/bubble-charts/bubble-charts.component.ts
@@ -6,11 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./bubble-charts.component.scss']
 })
 export class BubbleChartsComponent {
-
-  options: string[] = ["Option 1", "Option 2", "Option 3"]; // Mettez vos options réelles ici
-  menuVisible = false;
-
-  toggleMenu() {
-    this.menuVisible = !this.menuVisible;
-  }
 }
