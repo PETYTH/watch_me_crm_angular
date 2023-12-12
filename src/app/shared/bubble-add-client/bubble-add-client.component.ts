9e0b16b3cab3c0a39225db3bb72cfb80c2a4a87c
CEO-Niels <nielsbeaumont.etu@gmail.com>
2023-12-12 10:20:51 +0100

    bubble add et edit client

---
+++ src/app/shared/bubble-add-client/bubble-add-client.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bubble-add-client',
+  templateUrl: './bubble-add-client.component.html',
+  styleUrls: ['./bubble-add-client.component.scss']
+})
+export class BubbleAddClientComponent {
+
+}
