fb9aa316201b0cdb87ccc1933ccb1a3f6b9bc955
Christophe <kelevrap5@gmail.com>
2023-12-15 00:16:49 +0100

    modif titre tableau infos utile + insérer datas list

--- src/app/auth/api/api.service.ts
+++ src/app/auth/api/api.service.ts
@@ -238,4 +238,28 @@ export class ApiService {
     );
   }
 
+// USER //
+  getAllUsers(): Observable<any> {
+    const token = this.authService.getToken();
+
+    if (!token) {
+      console.error('Le token de session n\'a pas été trouvé.');
+      return new Observable(observer => {
+        observer.error('Token not found');
+        observer.complete();
+      });
+    }
+
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.get(`${this.apiUrl}/all_user`, { headers }).pipe(
+      catchError((error) => {
+        console.error('Une erreur est survenue');
+        return new Observable(observer => {
+          observer.error(error);
+          observer.complete();
+        });
+      })
+    );
+  }
+
 }
