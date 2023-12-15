105a68f2491299ab1beddc5d6914790c0d0b5dbd
Petyth_prince <petythprincebely@gmail.com>
2023-12-15 09:14:30 +0100

    Mise a jour des données chart

--- src/app/auth/connexion/page/form/form.component.ts
+++ src/app/auth/connexion/page/form/form.component.ts
@@ -5,74 +5,64 @@ import { ApiService } from '../../../api/api.service';
 import { AuthService } from '../../../../auth.service';
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+    selector: 'app-form',
+    templateUrl: './form.component.html',
+    styleUrls: ['./form.component.scss']
 })
 export class FormComponent {
-  credentialsForm: FormGroup;
-  loginError: string | undefined;
+    credentialsForm: FormGroup;
+    loginError: string | undefined;
 
-  constructor(
-    private fb: FormBuilder,
-    private apiService: ApiService,
-    private authService: AuthService,
-    private router: Router
-  ) {
-    this.credentialsForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
-    });
-  }
-
-  loginUser(): void {
-    const emailControl = this.credentialsForm.get('email');
-    const passwordControl = this.credentialsForm.get('password');
-
-    if (!emailControl?.value && passwordControl?.valid) {
-      // Affichage d'un message d'erreur si seul le mot de passe est saisi
-      this.loginError = 'Veuillez saisir l\'email';
-      return;
+    constructor(
+        private fb: FormBuilder,
+        private apiService: ApiService,
+        private authService: AuthService,
+        private router: Router
+    ) {
+        this.credentialsForm = this.fb.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', Validators.required]
+        });
     }
 
-    if (!passwordControl?.value && emailControl?.valid) {
-      // Affichage d'un message d'erreur si seul l'email est saisi
-      this.loginError = 'Veuillez saisir le mot de passe';
-      return;
-    }
+    loginUser(): void {
+        const emailControl = this.credentialsForm.get('email');
+        const passwordControl = this.credentialsForm.get('password');
 
-    if (emailControl && passwordControl && emailControl.valid && passwordControl.valid) {
-      const credentials = {
-        username: emailControl.value,
-        password: passwordControl.value,
-      };
-
-      this.apiService.loginUser(credentials).subscribe(
-        (response) => {
-          console.log('Connexion réussie :', response);
-          // Récupérer le premier élément du tableau de rôles
-          const role = response.roles && response.roles.length > 0 ? response.roles[0] : '';
-          // Obtenez la date d'expiration du token (ici, j'ajoute une heure à la date actuelle)
-          const tokenExpiry = new Date();
-          tokenExpiry.setHours(tokenExpiry.getHours() + 1);
-
-          // Utilisation de la méthode login de AuthService pour gérer la connexion et la déconnexion automatique
-          this.authService.login(response.token, response.lastName, response.firstName, role, tokenExpiry);
-
-          this.router.navigate(['/dashboard']);
-        },
-        (error) => {
-          console.error('Échec de la connexion :', error);
-
-          if (error.status === 0) {
-            this.loginError = 'La connexion à l\'API a été rompue';
-          } else if (error.status === 401) {
-            this.loginError = 'Vous n\'avez pas accès à ce compte';
-          } else {
-            this.loginError = error.error.message;
-          }
+        if (!emailControl?.value && passwordControl?.valid) {
+            this.loginError = 'Veuillez saisir l\'email';
+            return;
         }
-      );
+
+        if (!passwordControl?.value && emailControl?.valid) {
+            this.loginError = 'Veuillez saisir le mot de passe';
+            return;
+        }
+
+        if (emailControl && passwordControl && emailControl.valid && passwordControl.valid) {
+            const credentials = {
+                username: emailControl.value,
+                password: passwordControl.value,
+            };
+
+            this.apiService.loginUser(credentials).subscribe(
+                (response) => {
+                    const role = response.roles && response.roles.length > 0 ? response.roles[0] : '';
+                    const tokenExpiry = new Date();
+                    tokenExpiry.setHours(tokenExpiry.getHours() + 1);
+
+                    this.authService.login(response.token, response.lastName, response.firstName, role, tokenExpiry);
+
+                    // Déconnexion automatique si le token est expiré
+                    this.authService.tokenExpiryCheck();
+
+                    this.router.navigate(['/dashboard']);
+                },
+                (error) => {
+                    console.error('Échec de la connexion :', error);
+                    // Gestion des erreurs
+                }
+            );
+        }
     }
-  }
 }
--- src/app/shared/bubble-charts/bubble-charts.component.ts
+++ src/app/shared/bubble-charts/bubble-charts.component.ts
@@ -1,9 +1,129 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../auth/api/api.service';
+import { Chart, ChartType } from 'chart.js/auto';
+import { AuthService } from '../../auth.service'; // Assurez-vous que le chemin est correct
 
 @Component({
-  selector: 'app-bubble-charts',
-  templateUrl: './bubble-charts.component.html',
-  styleUrls: ['./bubble-charts.component.scss']
+    selector: 'app-bubble-charts',
+    templateUrl: './bubble-charts.component.html',
+    styleUrls: ['./bubble-charts.component.scss']
 })
-export class BubbleChartsComponent {
+export class BubbleChartsComponent implements OnInit {
+    entrepriseId: number | undefined;
+    selectedChartType: ChartType = 'doughnut';
+    chartTypes: ChartType[] = ['bar', 'line', 'radar', 'scatter', 'bubble', 'pie', 'doughnut', 'polarArea'];
+    clientsData: any = { labels: [], values: [] }; // Modification de la structure des données
+    chart: Chart | null = null;
+    entreprises: any[] = []; // Liste des entreprises
+
+    constructor(
+        private apiService: ApiService,
+        private authService: AuthService // Injectez le service d'authentification
+    ) { }
+
+    ngOnInit(): void {
+        this.loadEntreprises(); // Charger la liste des entreprises au chargement du composant
+    }
+
+    loadEntreprises(): void {
+        // Récupérer le token JWT depuis le service d'authentification
+        const token = this.authService.getToken();
+
+        // Vérifier si le token JWT est disponible
+        if (token) {
+            this.apiService.getAllEntreprises().subscribe(
+                (response) => {
+                    this.entreprises = response; // Stocker les entreprises récupérées
+                },
+                (error) => {
+                    console.error(error);
+                }
+            );
+        } else {
+            console.error('JWT Token not found'); // Gérer le cas où le token n'est pas disponible
+        }
+    }
+
+  onEntrepriseSelect(): void {
+    if (this.entrepriseId) {
+      const token = this.authService.getToken();
+
+      if (token) {
+        const headers = {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        };
+
+        this.apiService.count_clients_entreprise(this.entrepriseId, headers).subscribe(
+          (response: any) => {
+            const countsByStatus = response.countsByStatus;
+            if (Array.isArray(countsByStatus) && countsByStatus.length > 0) {
+              const firstData = countsByStatus[0]; // Prendre le premier élément du tableau
+              const twoData = countsByStatus[1]; // Prendre le premier élément du tableau
+              const threeData = countsByStatus[2]; // Prendre le premier élément du tableau
+              const fourData = countsByStatus[3]; // Prendre le premier élément du tableau
+              console.log(firstData);
+              console.log(twoData);
+              console.log(threeData);
+              console.log(fourData);
+
+              const labels = countsByStatus.map((item: any) => item.status);
+              const values = countsByStatus.map((item: any) => item.count);
+
+              this.clientsData = {
+                labels: labels,
+                values: values
+              };
+
+              this.updateChart();
+            } else {
+              console.error('Invalid data format or empty countsByStatus array');
+            }
+          },
+          (error) => {
+            console.error(error);
+          }
+        );
+      } else {
+        console.error('JWT Token not found');
+      }
+    }
+  }
+
+
+  updateChart(): void {
+    if (this.chart) {
+      this.chart.destroy();
+    }
+
+    const canvas = document.getElementById('myChart') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d');
+
+    if (ctx) {
+      const backgroundColors = ['red', 'orange', 'blue', 'green']; // Couleurs spécifiées
+
+      this.chart = new Chart(ctx, {
+        type: this.selectedChartType,
+        data: {
+          labels: this.clientsData.labels,
+          datasets: [{
+            data: this.clientsData.values,
+            backgroundColor: backgroundColors
+          }]
+        },
+        options: {
+          responsive: true,
+        }
+      });
+    }
+  }
+
+
+
+  onChartTypeSelect(event: Event): void {
+        const selectedType = (event.target as HTMLSelectElement).value as ChartType;
+        this.selectedChartType = selectedType;
+        this.updateChart();
+    }
 }
